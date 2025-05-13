3faea43979205f70e05fb99402cbe268666fc623
Vanshika Sharma <vanshikasharma01121995@gmail.com>
2025-05-13 15:38:55 +0530

    change

--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://green-cart-cyan.vercel.app",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
